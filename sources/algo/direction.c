124319486eceefe691dfd65e7593c850cf32e35c
Jean delaunay <jdelaunay@localhost.localdomain>
2018-05-14 15:43:06 +0200

    correct bug in direction : read -1

--- sources/algo/direction.c
+++ sources/algo/direction.c
@@ -23,18 +23,20 @@ float direction_car(int *lidar)
 {
 	float medium_l = 0;
 	float medium_r = 0;
-	float result = 1;
+	float result = 0;
+	float average = 0;
 
 	if (lidar == NULL) {
 		return (0);
 	}
 	medium_l = calc_medium(lidar[0], lidar[1], lidar[2], lidar[3]);
-	medium_r = calc_medium(lidar[29], lidar[30], lidar[31], lidar[32]);
+	medium_r = calc_medium(lidar[28], lidar[29], lidar[30], lidar[31]);
 	dprintf(2, "medium_l : %.2f medium_r : %.2f \n", medium_l, medium_r);
-	if (medium_l > medium_r) {
+	average = medium_l - medium_r;
+	if (average > 250) {
 		if (medium_l != 0)
 			result = (1 - (medium_r / medium_l));
-	} else {
+	} else if (average < -250) {
 		if (medium_r != 0)
 			result = (1 - (medium_l / medium_r));
 		result *= -1;
